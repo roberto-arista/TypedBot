c06ebd4d36abd86920739cdb2d52f5e03e7ae198
Roberto Arista <arista.rob@gmail.com>
2022-04-06 11:24:52 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='TypedBot',
-    author='Roberto Arista',
+    name="TypedBot",
+    author="Roberto Arista",
     author_email="hello@robertoarista.it",
-    version='0.1',
-    packages=['typedBot'],
+    version="0.2",
+    packages=["typedBot"],
     package_data={"typedBot": ["py.typed"]},
     include_package_data=True,
-    license=open('LICENSE').read(),
-    long_description=open('README.md').read(),
+    license=open("LICENSE").read(),
+    long_description=open("README.md").read(),
     platforms=["macOS"],
     python_requires=">=3.9",
 )
